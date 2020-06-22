096bdbb6e78643ae880e45a2139c3d067f80195d
koji314 <leamsi297@gmail.com>
2020-06-22 23:00:16 +1000

    Changed ST colorscheme

--- st/config.h
+++ st/config.h
@@ -8,8 +8,8 @@
 // Old AMI BIOS Font for te ~aesthetic~
 // static char *font = "PX437 AMI BIOS:pixelsize=13:antialias=true:autohint=true";
 // Menlo (monospace font used on modern MACOS, looks pretty nice.
-static char *font = "Menlo:pixelsize=13:antialias=true:autohint=true";
-
+// static char *font = "Menlo:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Terminus:pixelsize=13;antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -89,44 +89,44 @@ unsigned int tabspaces = 8;
 /* Everything nested in this statement here changes the colorscheme */
 static const char *colorname[] = {
 
-    /* 8 normal colors */
-    [0] = "#272822", /* black   */
-      [1] = "#f92672", /* red     */
-        [2] = "#a6e22e", /* green   */
-	  [3] = "#f4bf75", /* yellow  */
-	    [4] = "#66d9ef", /* blue    */
-	      [5] = "#ae81ff", /* magenta */
-	        [6] = "#a1efe4", /* cyan    */
-		  [7] = "#f8f8f2", /* white   */
+  /* 8 normal colors */
+  [0] = "#282a2e", /* black   */
+  [1] = "#a54242", /* red     */
+  [2] = "#8c9440", /* green   */
+  [3] = "#de935f", /* yellow  */
+  [4] = "#5f819d", /* blue    */
+  [5] = "#85678f", /* magenta */
+  [6] = "#5e8d87", /* cyan    */
+  [7] = "#707880", /* white   */
 
-		    /* 8 bright colors */
-		    [8]  = "#75715e", /* black   */
-		      [9]  = "#f92672", /* red     */
-		        [10] = "#a6e22e", /* green   */
-			  [11] = "#f4bf75", /* yellow  */
-			    [12] = "#66d9ef", /* blue    */
-			      [13] = "#ae81ff", /* magenta */
-			        [14] = "#a1efe4", /* cyan    */
-				  [15] = "#f9f8f5", /* white   */
+  /* 8 bright colors */
+  [8]  = "#373b41", /* black   */
+  [9]  = "#cc6666", /* red     */
+  [10] = "#b5bd68", /* green   */
+  [11] = "#f0c674", /* yellow  */
+  [12] = "#81a2be", /* blue    */
+  [13] = "#b294bb", /* magenta */
+  [14] = "#8abeb7", /* cyan    */
+  [15] = "#c5c8c6", /* white   */
 
-				    /* special colors */
-				    [256] = "#040404", /* background */
-				      [257] = "#f8f8f2", /* foreground */
+  /* special colors */
+  [256] = "#1b1d1e", /* background */
+  [257] = "#dddddd", /* foreground */
 };
 
 /*
- *  * Default colors (colorname index)
- *   * foreground, background, cursor
- *    */
+ * Default colors (colorname index)
+ * foreground, background, cursor
+ */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
+unsigned int defaultcs = 257;
 
 /*
- *  * Colors used, when the specific fg == defaultfg. So in reverse mode this
- *   * will reverse too. Another logic would only make the simple feature too
- *    * complex.
- *     */
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
 static unsigned int defaultitalic = 7;
 static unsigned int defaultunderline = 7;
 
