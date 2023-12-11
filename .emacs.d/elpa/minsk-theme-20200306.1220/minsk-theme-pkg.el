79055edcdf091bf2216107c3b35a88448666636f
Ismael Khan <ismael@ismaelkhan.com>
2023-12-11 22:58:27 +1000

    Removed .emacs.d superflous config

--- .emacs.d/elpa/minsk-theme-20200306.1220/minsk-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "minsk-theme" "20200306.1220" "Minsk, a theme in deep muted greens" '((emacs "24")) :commit "e4dcdec3a4472a507d6b249ae2194dacaa885ecb" :keywords '("theme" "faces") :authors '(("Jean Lo" . "jlpaca@users.noreply.github.com")) :maintainer '("Jean Lo" . "jlpaca@users.noreply.github.com") :url "https://github.com/jlpaca/minsk-theme")
--- .emacs.d/elpa/texfrag-20200716.1331/texfrag-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "texfrag" "20200716.1331" "preview LaTeX fragments in alien major modes" '((emacs "25") (auctex "11.90.2")) :commit "a5f59e0c5f43578f139a2943bd08e5b3140f4c2b" :keywords '("tex" "languages" "wp") :authors '(("Tobias Zawada" . "i@tn-home.de")) :maintainer '("Tobias Zawada" . "i@tn-home.de") :url "https://github.com/TobiasZawada/texfrag")
